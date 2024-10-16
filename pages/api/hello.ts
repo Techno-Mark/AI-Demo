507f658972dd4ac4d20483eb07f796917eed8391
vimalpatel-dev <vimal.patel@technomark.io>
2024-10-16 16:35:32 +0530

    add redirection to  '/product-recommendation'

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -12,6 +12,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   if (req.method === 'POST') {
     const bodyData = req.body; 
     data = bodyData;           
+    return res.redirect('/product-recommendation');
     res.status(200).json({ message: 'Ok' });
   } else {
     res.status(200).json({ message: 'Ok', data: data ? (data) : 'No data received yet' });
