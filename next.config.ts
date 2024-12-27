cf669b42b3adddc754166e05eb52c22d84d21881
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2024-12-27 16:59:15 +0530

    next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    APIKEY: process.env.APIKEY,
+    APIBASE: process.env.APIBASE,
+    APIVERSION: process.env.APIVERSION,
+    DEPLOYMENTNAME: process.env.DEPLOYMENTNAME,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
