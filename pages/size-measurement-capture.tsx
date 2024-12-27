34a4777c1d3dbf99a2fd4a3743395964a9c14cf8
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2024-12-27 17:06:58 +0530

    cm sizes added

--- pages/size-measurement-capture.tsx
+++ pages/size-measurement-capture.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Header from "tsconfig.json/components/Header`";
 import SizeCapture1 from "tsconfig.json/components/SizeMeasurment/SizeCapture1`";
 import SizeCapture from "tsconfig.json/components/SizeMeasurment/SizeCapture`";
@@ -5,7 +6,11 @@ export default function Home() {
   return (
     <>
       <main className={`min-h-screen`}>
-        <Header />
+      <div className="py-2.5 border-b">
+        <div className="mx-auto px-20">
+          <Image src="https://technomark.io/images/logo.svg" width={150} height={150} alt="Logo" />
+        </div>
+      </div>
         <SizeCapture1 />
       </main>
     </>
