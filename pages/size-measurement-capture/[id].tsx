6c8efc4ebc168215df20da2a76a0ec882ed526b8
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2025-01-24 10:34:15 +0530

    admin pannel added

--- pages/size-measurement-capture/[id].tsx
+++ pages/size-measurement-capture/[id].tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import Logo from "tsconfig.json/assets/icons/Logo`";
+import Admin from "tsconfig.json/components/SizeMeasurment/Admin`";
 import FindSize from "tsconfig.json/components/SizeMeasurment/Fitcheck/FindSize`";
 import SizeCapture1 from "tsconfig.json/components/SizeMeasurment/SizeCapture1`";
 import SizeCapture2 from "tsconfig.json/components/SizeMeasurment/SizeCapture2`";
@@ -22,31 +23,35 @@ const id = () => {
           : "bg-white"
       } text-black`}
     >
-      <div className="py-2.5 border-b">
-        <div className="mx-auto px-8 lg:px-20">
-          {params === "/size-measurement-capture/fitcheck" ? (
-            <Logo />
-          ) : (
-            <Image
-              src="https://technomark.io/images/logo.svg"
-              width={150}
-              height={150}
-              alt="Logo"
-            />
-          )}
+      {params !== "/size-measurement-capture/admin" && (
+        <div className="py-2.5 border-b">
+          <div className="mx-auto px-8 lg:px-20">
+            {params === "/size-measurement-capture/fitcheck" ? (
+              <Logo />
+            ) : (
+              <Image
+                src="https://technomark.io/images/logo.svg"
+                width={150}
+                height={150}
+                alt="Logo"
+              />
+            )}
+          </div>
         </div>
-      </div>
-      {params !== "/size-measurement-capture/fitcheck" && (
-        <p
-          className="my-4 ml-5 cursor-pointer"
-          onClick={() => router.push("/size-measurement-capture")}
-        >
-          &lt; Back
-        </p>
       )}
+      {params !== "/size-measurement-capture/fitcheck" &&
+        params !== "/size-measurement-capture/admin" && (
+          <p
+            className="my-4 ml-5 cursor-pointer"
+            onClick={() => router.push("/size-measurement-capture")}
+          >
+            &lt; Back
+          </p>
+        )}
       {params === "/size-measurement-capture/v1" && <SizeCapture1 />}
       {params === "/size-measurement-capture/v2" && <SizeCapture4 />}
       {params === "/size-measurement-capture/fitcheck" && <FindSize />}
+      {params === "/size-measurement-capture/admin" && <Admin />}
       {params === "/size-measurement-capture/test" && <SizeCapture5 />}
     </main>
   );
