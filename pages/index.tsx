c95d65f1db96526934c06056f3d77a4074bd10fb
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2025-04-15 10:42:44 +0530

    product recommandation hide

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const HomePage = () => {
           </button>
         </Link>
 
-        <Link href="/product-recommendation" passHref>
+        {/* <Link href="/product-recommendation" passHref>
           <button className="flex items-center bg-white rounded-full px-4 py-2 shadow-md hover:scale-105 transform transition duration-200 ease-in-out cursor-pointer border border-gray-200">
             <FontAwesomeIcon
               icon={faLightbulb}
@@ -68,7 +68,7 @@ const HomePage = () => {
               Product Recommendation
             </span>
           </button>
-        </Link>
+        </Link> */}
 
         <Link href="/chat-summary" passHref>
           <button className="flex items-center bg-white rounded-full px-4 py-2 shadow-md hover:scale-105 transform transition duration-200 ease-in-out cursor-pointer border border-gray-200">
