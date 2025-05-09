7c2b6d2efc9b085714e997738a6e2bdaaee4433f
HarshTechnomark <127918058+HarshTechnomark@users.noreply.github.com>
2025-05-09 14:46:53 +0530

    size changes

--- components/SizeMeasurment/Fitcheck/FitCheckYourSize3.tsx
+++ components/SizeMeasurment/Fitcheck/FitCheckYourSize3.tsx
@@ -1097,14 +1097,14 @@ const FitCheckYourSize4 = ({
                       </Typography> */}
                       {capturedImage && isCounting && (
                         <Typography variant="h6" color="primary">
-                          <span className="text-lg md:text-4xl lg:text-5xl font-bold">
+                          <span className="text-2xl md:text-6xl lg:text-7xl font-bold">
                             ROTATE TO THE SIDE
                           </span>
                         </Typography>
                       )}
                       {isCounting && (
                         <Typography variant="h6" color="primary">
-                          <span className="text-lg md:text-4xl lg:text-5xl font-bold">
+                          <span className="text-2xl md:text-6xl lg:text-7xl font-bold">
                             Countdown: {countdown} seconds
                           </span>
                         </Typography>
@@ -1112,7 +1112,7 @@ const FitCheckYourSize4 = ({
                     </>
                   ) : (
                     <Typography variant="h6" color="error">
-                      <span className="text-lg md:text-4xl lg:text-5xl font-bold">
+                      <span className="text-2xl md:text-6xl lg:text-7xl font-bold">
                         No user detected. Please step into the frame.
                       </span>
                     </Typography>
@@ -1120,7 +1120,7 @@ const FitCheckYourSize4 = ({
 
                   {userDetected && errorMessage && (
                     <Typography variant="h6" color="error">
-                      <span className="text-lg md:text-4xl lg:text-5xl font-bold">
+                      <span className="text-2xl md:text-6xl lg:text-7xl font-bold">
                         {errorMessage}
                       </span>
                     </Typography>
@@ -1129,7 +1129,7 @@ const FitCheckYourSize4 = ({
               </div>
             ) : (
               <Typography variant="h6" color="error">
-                <span className="text-lg md:text-4xl lg:text-5xl font-bold">No Camera Found</span>
+                <span className="text-2xl md:text-6xl lg:text-7xl font-bold">No Camera Found</span>
               </Typography>
             )}
 
